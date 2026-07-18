402925924600fc43cc5a51bd168fc1c13004dea4
KanakaNakazawa <tgigroupid@gmail.com>
2026-07-18 15:58:21 +0700

    feat [docs - vitepress & typedoc] Add vitepress and typedoc

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitepress';
+
+export default defineConfig({
+  title: 'Lumine Chat',
+  description: 'Satu library untuk semua chat platform.',
+  themeConfig: {
+    nav: [
+      { text: 'Panduan', link: '/guide/getting-started' },
+      { text: 'API', link: '/api/index' },
+      { text: 'GitHub', link: 'https://github.com/lumine-lib/lumine-chat' }
+    ],
+    sidebar: {
+      '/guide/': [
+        { text: 'Memulai', link: '/guide/getting-started' },
+        { text: 'Platform', link: '/guide/platform' },
+      ],
+      '/api/': [
+        { text: 'API Reference', link: '/api/index' }
+      ],
+    }
+  }
+});
